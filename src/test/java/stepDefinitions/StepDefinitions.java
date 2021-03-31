0f13b4233803a4ce3408debfb8d784fd98b064cf
JohanSvast <nv95jhle@hotmail.com>
2021-03-31 17:51:36 +0200

    Third commit

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -3,13 +3,18 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertEquals;
+
 public class StepDefinitions {
+
     private WebDriver driver;
+
     @Given("I want to add my email {string}")
     public void i_want_to_add_my_email(String string) {
         // Write code here that turns the phrase above into concrete actions
@@ -25,10 +30,25 @@ public class StepDefinitions {
     @Given("I also want to set an username {string}")
     public void i_also_want_to_set_an_username(String string) {
         // Write code here that turns the phrase above into concrete actions
-        WebElement username = driver.findElement(By.id("new_username"));
-        username.sendKeys(string);
+        if (string.equals("randUser")) {
+            WebElement username = driver.findElement(By.id("new_username"));
+            username.sendKeys(RandomStringUtils.randomAlphabetic(12));
 
+        }
+        else if (string.equals("randUser100")){
+            WebElement username = driver.findElement(By.id("new_username"));
+            username.sendKeys(RandomStringUtils.randomAlphabetic(110));
+        }
+        else if (string.equals("oldUser")){
 
+            WebElement username = driver.findElement(By.id("new_username"));
+            String nameUser = (RandomStringUtils.randomAlphabetic(12));
+            username.sendKeys(nameUser);
+        }
+        else {
+            WebElement username = driver.findElement(By.id("new_username"));
+            username.sendKeys(RandomStringUtils.randomAlphabetic(12));
+        }
     }
 
     @Given("I want to set a Password {string}")
@@ -36,7 +56,8 @@ public class StepDefinitions {
         // Write code here that turns the phrase above into concrete actions
         WebElement password = driver.findElement(By.id("new_password"));
         password.sendKeys(string);
-
+        WebElement pop = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        pop.click();
     }
 
     @When("I press signup")
@@ -47,10 +68,19 @@ public class StepDefinitions {
 
     }
 
-    @Then("The {string} should be that i see a confirm page.")
-    public void the_result_should_be_that_i_see_a_confirm_page(String string) {
+    @Then("When user is {string} then the {string} should be.")
+    public void when_user_is_then_the_should_be(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-
+        if (string.equals("randUser")){
+            WebElement firstResult = driver.findElement(By.cssSelector("#signup-content > div > div > div > span > label"));
+            assertEquals(string2, firstResult.getText());
+        }
+       else{
+            WebElement secResult = driver.findElement(By.cssSelector("#av-flash-errors > ul"));
+            assertEquals(string2, secResult.getText());
+        }
+        driver.quit();
     }
+
 
 }
--- src/test/resources/Features/Scenario.feature
+++ src/test/resources/Features/Scenario.feature
@@ -9,7 +9,10 @@ Feature: MailChimp
     Given I also want to set an username "<username>"
     And  I want to set a Password "<password>"
     When I press signup
-    Then The "<result>" mail should be on the confirm page.
+    Then When user is "<username>" then the "<result>" should be.
     Examples:
-      | email | username | password | result |
-      | jst@fun.com | jstfun | H4rdP@ssW0rd | jst@fun.com |
+      | email             | username    | password     | result |
+      | newuser@test.com  | randUser    | H4rdP@ssW0rd | Didn’t get an email? |
+      | longuser@test.com | randUser100 | H4rdP@ssW0rd | Please check your entry and try again. |
+      | olduser@test.com  | oldUser     | H4rdP@ssW0rd | Please check your entry and try again. |
+      |                   | userName    | H4rdP@ssW0rd | Please check your entry and try again. |
