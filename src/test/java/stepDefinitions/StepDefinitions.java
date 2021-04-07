05f06990a60d5817448d161fba2201ff13bfe260
JohanSvast <nv95jhle@hotmail.com>
2021-04-07 10:49:22 +0200

    5th release

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -14,10 +14,11 @@ import static org.junit.Assert.assertEquals;
 public class StepDefinitions {
 
     private WebDriver driver;
+    Wait sendKeys = new Wait();
+    Wait click = new Wait();
 
     @Given("I want to add my email {string}")
     public void i_want_to_add_my_email(String string) {
-        // Write code here that turns the phrase above into concrete action
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\nv95j\\OneDrive\\Skrivbord\\Selenium\\chromedriver.exe");
 
         driver = new ChromeDriver();
@@ -31,53 +32,44 @@ public class StepDefinitions {
     @Given("I also want to set an username {string}")
     public void i_also_want_to_set_an_username(String string) {
         // Write code here that turns the phrase above into concrete actions
-        if (string.equals("randUser")) {
-            WebElement username = driver.findElement(By.id("new_username"));
-            username.sendKeys(RandomStringUtils.randomAlphabetic(12));
-        } else if (string.equals("randUser100")) {
-            WebElement username = driver.findElement(By.id("new_username"));
-            username.sendKeys(RandomStringUtils.randomAlphabetic(110));
-        } else if (string.equals("oldUser")) {
-            WebElement username = driver.findElement(By.id("new_username"));
-            // String nameUser = (RandomStringUtils.randomAlphabetic(12));
-            username.sendKeys("oldUser");
-        } else {
-            WebElement username = driver.findElement(By.id("new_username"));
-            username.sendKeys(RandomStringUtils.randomAlphabetic(12));
+        switch (string) {
+            case "randUser" -> sendKeys.sendKeys(driver, By.id("new_username"), (RandomStringUtils.randomAlphabetic(12)));
+            case "randUser100" -> sendKeys.sendKeys(driver, By.id("new_username"), (RandomStringUtils.randomAlphabetic(110)));
+            case "oldUser" -> sendKeys.sendKeys(driver, By.id("new_username"), ("oldUser"));
+            default -> sendKeys.sendKeys(driver, By.id("new_username"), (RandomStringUtils.randomAlphabetic(13)));
         }
     }
 
     @Given("I want to set a Password {string}")
     public void i_want_to_set_a_password(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        WebElement password = driver.findElement(By.id("new_password"));
-        password.sendKeys(string);
-        WebElement pop = driver.findElement(By.id("onetrust-accept-btn-handler"));
-        pop.click();
+        sendKeys.sendKeys(driver, By.id("new_password"), (string));
+        click.click(driver, By.id("onetrust-accept-btn-handler"));
     }
 
     @When("I press signup")
     public void i_press_signup() {
-        // Write code here that turns the phrase above into concrete actions
-        WebElement signUp = driver.findElement(By.cssSelector("#create-account"));
-        signUp.click();
+        click.click(driver, By.cssSelector("#create-account"));
     }
 
     @Then("When user is {string} then the {string} should be.")
     public void when_user_is_then_the_should_be(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        if (string.equals("randUser")) {
-            WebElement firstResult = driver.findElement(By.cssSelector("#signup-content > div > div > div > span > label"));
-            assertEquals(string2, firstResult.getText());
-        } else if (string.equals("randUser100")) {
-            WebElement firstResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span"));
-            assertEquals(string2, firstResult.getText());
-        } else if (string.equals("oldUser")) {
-            WebElement firstResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span"));
-            assertEquals(string2, firstResult.getText());
-        } else {
-            WebElement secResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(1) > div > span"));
-            assertEquals(string2, secResult.getText());
+        switch (string) {
+            case "randUser" -> {
+                WebElement firstResult = driver.findElement(By.cssSelector("#signup-content > div > div > div > span > label"));
+                assertEquals(string2, firstResult.getText());
+            }
+            case "randUser100" -> {
+                WebElement firstResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span"));
+                assertEquals(string2, firstResult.getText());
+            }
+            case "oldUser" -> {
+                WebElement firstResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(2) > div > span"));
+                assertEquals(string2, firstResult.getText());
+            }
+            default -> {
+                WebElement secResult = driver.findElement(By.cssSelector("#signup-form > fieldset > div:nth-child(1) > div > span"));
+                assertEquals(string2, secResult.getText());
+            }
         }
         driver.quit();
     }
